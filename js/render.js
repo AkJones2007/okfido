08074e08541ee60e441bd5745aee20679b86e364
Alex <AkJones2007@gmail.com>
2015-11-12 07:50:11 -0500

    Replaces '=' with ':' in the formStatus method definition, because it's part of an object you idiot.

--- js/render.js
+++ js/render.js
@@ -1,7 +1,7 @@
 // This file handles all HTML rendering in the DOM
 var render = {
 
-  formStatus = function(message) {
+  formStatus: function(message) {
     $('.register').append('<h3>' + message + '</h3>');
   }
 
