53dfb9edd4b112d021dfd4e4935e32c86b468d3f
Alex <AkJones2007@gmail.com>
2015-11-11 22:01:24 -0500

    Creates function to convert form data into an object

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,14 @@
 // This file contains arbitrary helper functions to be
 // used throughout the program
+
+// Convert a form to an object
+var objectifyForm = function objectifyForm(form) {
+  var object = {};
+  $(form).children().each(function(index, element) {
+    var type = $(this).attr('type');
+    if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+      object[$(this).attr('name')] = $(this).val();
+    }
+  });
+  return object;
+};
