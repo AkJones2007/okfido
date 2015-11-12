60c23962f6893060ff6953fb7cd8cdf8ef4ead7f
Alex <AkJones2007@gmail.com>
2015-11-11 21:43:57 -0500

    Creates OkFido API object
    Adds user registration and login ajax methods

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,29 @@
 // This file handles all http requests
+var okfidoAPI = {
+  // Set URL
+  apiurl: "http://localhost:3000/",
+  ajax: $.ajax,
+
+  // Register
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.apiurl + 'register',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    });
+  },
+
+  // Login
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.apiurl + 'login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    });
+  }
+
+};
