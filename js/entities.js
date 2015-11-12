fcb666a09d2b1d86956b8155c3744dc58400876c
Alex <AkJones2007@gmail.com>
2015-11-12 10:11:33 -0500

    Creates 'entity' object to hold data returned from the server

--- js/entities.js
+++ js/entities.js
@@ -1,12 +1,9 @@
 // This file contains object constructors
 // for each entity in the application
 
-// User
-var User = function User(firstName, lastName, email, city, state, token) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.city = city;
-  this.state = state;
-  this.token = token;
+var entity = {
+
+  // User
+  user: null
+
 };
