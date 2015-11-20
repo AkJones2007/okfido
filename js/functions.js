81ea9257b13a69c10a8df3f3b7ebfc3ab8cdf740
Alex <AkJones2007@gmail.com>
2015-11-20 18:32:02 -0500

    Adds a function to handle search filters.

--- js/functions.js
+++ js/functions.js
@@ -20,76 +20,11 @@ var wrap = function wrap(key, data) {
   return wrapped;
 };
 
-// Calculate a dog's age
-var getAge = function getAge(dob) {
-    today = new Date();
-    dob = new Date(dob);
-    age = today.getFullYear() - dob.getFullYear();
-
-    dob.setFullYear(2015);
-
-    if (today < dob) {
-        age--;
+var filterMatch = function filterMatch(options, searchResult) {
+  for (var key in options) {
+    if(options[key] !== searchResult[key]) {
+      return false;
     }
-
-    return age;
-};
-
-// Classify a dog by age (ex. Young)
-var classifyAge = function classifyAge(age) {
-
-    if (age < 1) {
-      return 'Baby';
-    }
-    else if(age < 4) {
-      return 'Young';
-    }
-    else if(age < 8) {
-      return 'Adult';
-    }
-    else if(age >= 8) {
-      return 'Senior'
-    }
-    else {
-      return null;
-    }
-
-};
-
-// Classify a dog by size (ex. Large)
-var classifySize = function classifySize(size) {
-
-  switch(size) {
-    case 1:
-      return 'Very Small';
-    case 2:
-      return 'Small';
-    case 3:
-      return 'Medium';
-    case 4:
-      return 'Large';
-    case 5:
-      return 'Very Large'
-    default:
-      return null;
   }
-
-};
-
-var dogProfile = function dogProfile(dog) {
-  var image = '<img src="images/placeholder.jpg" class="result-photo left">';
-  var name = dog.name;
-  var breed = request.breedMix(dog.id, response.dogList);
-  var age = classifyAge(getAge(dog.dob));
-  var size = classifySize(dog.size);
-  var gender = dog.gender;
-  var color = request.colorMix(dog.id, response.dogList);
-  var bio = dog.bio;
-
-  return '<li><div>' + image + '</div>' +
-         "<div class='result-content'>" +
-         '<h1>' + name + '</h1>' +
-         '<h2>' + breed + ' / ' + age + ' / ' + size +
-         ' / ' + gender + ' / ' + color + '</h2>' +
-         '<p>' + bio + '</p></div></li>';
+  return true;
 };
