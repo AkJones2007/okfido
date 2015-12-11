255ef996353cb936e0777491c84244a5174b2f56
Alex <AkJones2007@gmail.com>
2015-12-11 15:18:47 -0500

    Forgot the slash idiot.

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,7 @@
 // Information about the api
 var api = {
   // Set URL
-  url: "https://floating-savannah-7491.herokuapp.com",
+  url: "https://floating-savannah-7491.herokuapp.com/",
 
   // Configure AJAX
   ajax: function(config, cb) {
